5cb4de9a40757d3676bcadb35c4846c736492c69
HoangBenh <luuvanhoang2k4@gmail.com>
2024-12-04 10:53:42 +0700

    hoang2k4

--- src/Views/Client/cart.blade.php
+++ src/Views/Client/cart.blade.php
@@ -200,7 +200,7 @@
                             </tfoot>
                         </table>
                         <div class="checkout-methods">
-                            <a href="#" class="btn btn-block btn-dark">
+                            <a href="{{ routeClient('checkout')}}" class="btn btn-block btn-dark">
                                 Proceed to Checkout
                                 <i class="fa fa-arrow-right"></i>
                             </a>
